13ed6bbfdf3f80bd81a269ced875daacb89b8829
za-rth <anothertier@gmail.com>
2025-10-28 23:03:19 +0800

    Staged

--- app/page.tsx
+++ app/page.tsx
@@ -2,30 +2,35 @@
 import Image from "next/image";
 import Footer from "@/components/FootSection"
 import Header from "@/components/HeadSection"
-import {motion} from "framer-motion";
+import LeftsideBar from "@/components/LeftSideBar"
+import { motion } from "framer-motion";
 export default function Home() {
   return (
 
-    <div>
-      <Header/>
-      
+    <div className="flex flex-col">
+      <div className="top-0 left-0   h-screen w-[1rem] bg-white shadow-md p-[25] flex flex-col">
+      </div>
+
+      <Header />
+
+
       <section>
-        
+
         <div className="w-full flex items-center justify-center p-50 ">
-          
+
           <div className="flex sm:h-[60vh] sm:w-[500wh] w-500 h-100 justify-center border-2 border-white-600  rounded
              bg-linear-to-tr from-blue-600 to-fuchsia-500
           ">
-          <motion.div>
-           
+            <motion.div>
 
-          </motion.div>
+
+            </motion.div>
 
           </div>
           <div></div>
         </div>
       </section>
-      <Footer/>
+      <Footer />
     </div>
   );
 }
---
+++ components/LeftSideBar.tsx
@@ -0,0 +1,5 @@
+export default function LeftsideBar(){
+    return(
+<></>
+    )
+}
