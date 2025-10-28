690e8776e2ace5b4c489a1658ca343b06d6bf42d
za-rth <anothertier@gmail.com>
2025-10-28 23:35:44 +0800

    Conflict File

--- components/LeftSideBar.tsx
+++ components/LeftSideBar.tsx
@@ -1,5 +1,12 @@
-export default function LeftsideBar(){
-    return(
-<></>
+export default function LeftsideBar() {
+    return (
+        <div className="top-0 left-0   h-screen w-[1rem] bg-white shadow-md p-[25] flex flex-col">
+            <div className="flex flex-row items-start text-black overflow-visible rotate-90">
+                <a href="#">LINK</a>
+                <a href="#">LINK</a>
+                <a href="#">LINK</a>
+            </div>
+        </div>
+
     )
 }
---
+++ components/Main.tsx
@@ -0,0 +1,21 @@
+import {motion} from "framer-motion";
+export default function MainLayout() {
+    return (
+          <section>
+
+        <div className="w-full flex items-center justify-center p-50 ">
+
+          <div className="flex sm:h-[60vh] sm:w-[500wh] w-500 h-100 justify-center border-2 border-white-600  rounded
+             bg-linear-to-tr from-blue-600 to-fuchsia-500
+          ">
+            <motion.div>
+
+
+            </motion.div>
+
+          </div>
+          <div></div>
+        </div>
+      </section>
+    )
+}
