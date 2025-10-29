e4955347b5a77b6d49e0bdca8ddab9af8524b19f
za-rth <anothertier@gmail.com>
2025-10-29 11:34:12 +0800

    Staged

--- components/LeftSideBar.tsx
+++ components/LeftSideBar.tsx
@@ -1,10 +1,15 @@
+import { motion } from "framer-motion";
+
 export default function LeftsideBar() {
     return (
         <div className="top-0 left-0   h-screen w-[1rem] bg-white shadow-md p-[25] flex flex-col">
-            <div className="flex flex-row items-start text-black overflow-visible rotate-90">
-                <a href="#">LINK</a>
-                <a href="#">LINK</a>
-                <a href="#">LINK</a>
+            <div className="flex justify-between flex-col items-start text-black overflow-visible rotate-90 ">
+                <motion.div className="overflow-hidden w-64 cursor-pointer font-pixelify text-2xl">
+                    <motion.span
+                    >
+                       </motion.span>
+                    
+                </motion.div>
             </div>
         </div>
 
